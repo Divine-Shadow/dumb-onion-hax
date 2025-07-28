229e363db3437626a52e2ea4a2c68461b4bb2754
Bill Molchan <wmolchan@hawk.iit.edu>
2025-07-28 14:11:27 -0400

    [Refactor] Extract map upload service

---
+++ apps/src/main/scala/com/crib/bills/dom6maps/services/map/Impl.scala
@@ -0,0 +1,24 @@
+package com.crib.bills.dom6maps
+package apps.services.map
+
+import cats.effect.Async
+import cats.syntax.all.*
+import fs2.Stream
+import java.nio.charset.StandardCharsets
+import model.map.*
+import model.map.Renderer.*
+
+trait Impl[Sequencer[_]] extends Service[Sequencer]:
+  protected given sequencer: Async[Sequencer]
+
+  override def processUpload(request: MapUploadRequest): Sequencer[String] =
+    for
+      bytes <- sequencer.delay(request.map.getBytes(StandardCharsets.UTF_8))
+      directives <- MapFileParser
+                      .parse[Sequencer]
+                      .apply(Stream.emits(bytes).covary[Sequencer])
+                      .compile
+                      .toVector
+      rest = directives.filterNot(_.isInstanceOf[MapSize])
+      newDirectives = MapSize(request.config.mapSize.width, request.config.mapSize.height) +: rest
+    yield newDirectives.map(_.render).mkString("\n")
